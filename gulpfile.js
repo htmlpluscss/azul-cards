5588b4a362fadaa04fa7a8d8be71afb1569b11c3
opopopopopj <mixkovrigin@yandex.by>
2025-10-21 22:45:25 +0300

    footer

--- gulpfile.js
+++ gulpfile.js
@@ -75,16 +75,16 @@ const html = (files, since = {}, folder = '') => {
 		.pipe(replace('	', '  '))
 		.pipe(w3cjs({
 			url : 'https://validator.w3.org/nu/',
-		 	verifyMessage: (type, message) => {
+			verifyMessage: (type, message) => {
 
-		 		// prevent logging error message
-		 		if(message.includes('for attribute “src” on element “img”')) return false;
+				// prevent logging error message
+				if(message.includes('for attribute “src” on element “img”')) return false;
 
-		 		if(message.includes('iframe')) return false;
+				if(message.includes('iframe')) return false;
 
-		 		// allow message to pass through
-		 		return true;
-		 	}
+				// allow message to pass through
+				return true;
+			}
 		}))
 		.pipe(w3cjs.reporter())
 		.pipe(gulp.dest('build' + folder))
